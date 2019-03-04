5cc831f3353a7a31c0a204daa7d243692c489a6f
milespucarelli <miles.pucarelli@gmail.com>
2019-03-04 18:23:54 -0500

    routing for houses

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :characters, only: :index
+      get '/regions/:house', to: 'characters#houses'
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Character.destroy_all
+
 filtered_response = GameOfThronesApi.get_characters.select do |character|
-  character["playedBy"] != [""] && character["tvSeries"].length > 3
+  character['playedBy'] != [''] && character['tvSeries'].length > 3
 end
 
 filtered_response.each do |character|
+  house = { 'name' => 'No house' }
+  if character['allegiances'].length.positive?
+    house = GameOfThronesApi.get_houses.find do |h|
+      h['url'] == character['allegiances'][0]
+    end
+  end
+
   Character.create(
     name: character['name'],
-    house: character['house'],
+    house: house['name'],
     gender: character['gender'],
     culture: character['culture'],
     playedBy: character['playedBy'][0]
