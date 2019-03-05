13f0d737b16555a4d787c9d3affa593394f68fce
github username <github email address>
2019-03-05 15:23:04 -0500

    filtered api

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -4,10 +4,21 @@ class Api::V1::CharactersController < ApplicationController
     render json: @characters
   end
 
+
   def houses
+    character = Character.all.map {|character| character.url}
     @houses = GameOfThronesApi.get_houses.select do |house|
       house['region'] == params[:house].split('_').join(' ')
     end
+
+    @houses = @houses.select do |house|
+      character & house['swornMembers'] != []
+    end
+
+    keys = ['url', 'name', 'region', 'coatOfArms', 'words', 'seats', 'swornMembers']
+
+    @houses = @houses.map { |house| house.slice(*keys) }
+
     render json: @houses
   end
 end
